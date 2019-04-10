7a637cb36c9bf66c69cd7f28e5a2c92993d4cbff
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-04-10 21:57:59 +1000

    Created controller, linked to fxml. Seems to be working.

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,4 +1,85 @@
 package main.controller;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import main.model.Attributes;
+import main.model.Exceptions.AttributeCountException;
+import main.model.Exceptions.NoMatchException;
+import main.model.Model;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    
+    private Model model;
+    
+    public void setModel(Model model) {
+        this.model = model;
+    }
+    
+    @FXML // fx:id="legsCombo"
+    private ComboBox<Enum> legsCombo;
+    
+    @FXML // fx:id="wingsCombo"
+    private ComboBox<Enum> wingsCombo;
+    
+    @FXML // fx:id="flyCombo"
+    private ComboBox<Enum> flyCombo;
+    
+    @FXML// fx:id="tailCombo"
+    private ComboBox<Enum> tailCombo;
+    
+    @FXML // fx:id="natureCombo"
+    private ComboBox<Enum> natureCombo;
+    
+    @FXML // fx:id="habitatCombo"
+    private ComboBox<Enum> habitatCombo;
+    
+    @FXML // fx:id="activeCombo"
+    private ComboBox<Enum> activeCombo;
+    
+    @FXML // fx:id="resultField"
+    private TextField resultField;
+    
+    @FXML // fx:id="searchButton"
+    private Button searchButton;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        legsCombo.setItems(FXCollections.observableArrayList(Attributes.Legs.values()));
+        wingsCombo.setItems(FXCollections.observableArrayList(Attributes.Feature.values()));
+        flyCombo.setItems(FXCollections.observableArrayList(Attributes.Feature.values()));
+        tailCombo.setItems(FXCollections.observableArrayList(Attributes.Feature.values()));
+        natureCombo.setItems(FXCollections.observableArrayList(Attributes.Nature.values()));
+        habitatCombo.setItems(FXCollections.observableArrayList(Attributes.Habitat.values()));
+        activeCombo.setItems(FXCollections.observableArrayList(Attributes.Active.values()));
+        
+        searchButton.setOnAction(event -> {
+            List<Enum> attributes = new ArrayList<>();
+            attributes.add(legsCombo.getSelectionModel().getSelectedItem());
+            attributes.add(wingsCombo.getSelectionModel().getSelectedItem());
+            attributes.add(flyCombo.getSelectionModel().getSelectedItem());
+            attributes.add(tailCombo.getSelectionModel().getSelectedItem());
+            attributes.add(natureCombo.getSelectionModel().getSelectedItem());
+            attributes.add(habitatCombo.getSelectionModel().getSelectedItem());
+            attributes.add(activeCombo.getSelectionModel().getSelectedItem());
+            
+            String result = "No matches";
+            try {
+                result = model.search(attributes);
+            } catch (AttributeCountException | NoMatchException e) {
+                e.printStackTrace();
+            }
+            resultField.setText(result);
+        });
+    }
 }
--- src/main/view/IntegradevApplication.java
+++ src/main/view/IntegradevApplication.java
@@ -5,18 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.controller.Controller;
+import main.model.Model;
 
 public class IntegradevApplication extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("layouts/IntegradevSearcher.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("layouts/IntegradevSearcher.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.setModel(new Model());
         primaryStage.setTitle("Integradev Interview Application");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
