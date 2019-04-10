5293e8c1bc4429b2cabe342836335a69f9ab7900
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-04-10 21:06:36 +1000

    Created simple view

--- src/main/view/IntegradevApplication.java
+++ src/main/view/IntegradevApplication.java
@@ -5,18 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import main.model.Loader.Loader;
-import main.model.Searchable;
-
-import java.util.List;
 
 public class IntegradevApplication extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main/view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("layouts/IntegradevSearcher.fxml"));
+        primaryStage.setTitle("Integradev Interview Application");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
